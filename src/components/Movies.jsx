2f9f11f286c3e4102009b3319d821e62322322ba
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2022-07-24 13:37:33 +0300

    project complite

---
+++ src/components/Movies.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Movie } from "./Movie";
+
+function Movies(props) {
+  const { movies = [] } = props;
+
+  return (
+    <div className="movies">
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)
+      ) : (
+        <h5>Nothing found</h5>
+      )}
+    </div>
+  );
+}
+
+export { Movies };

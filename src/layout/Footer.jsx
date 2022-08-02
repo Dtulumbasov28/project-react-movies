e9702cefd4e2cf8b5528cb7955debc46a3de63a9
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2022-08-02 12:40:48 +0300

    Исправлены ссылки на репозиторий; изменен файл readme

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,7 +6,12 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="https://github.com/Dtulumbasov28/project-react-movies">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/Dtulumbasov28/project-react-movies"
+            target="_blank"
+            rel="noreferrer"
+          >
             Repo
           </a>
         </div>
